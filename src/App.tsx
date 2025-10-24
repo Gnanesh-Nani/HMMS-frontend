3e31084d68f31c1e6878aab19c70fb4316e4179d
Kamalakannan <kannansamy780@gmail.com>
2025-10-24 20:50:46 +0530

    otp verification

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import StudentDashboard from "./pages/StudentDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
+import FirstTimeChangePassword from "./pages/change-password";
 import ProtectedRoute from "./components/ProtectedRoute";
-// import ChangePasswordPage from "./pages/ChangePassword"; // ✅ Add this route 
 import { AuthProvider } from "./context/AuthContext";
 
 export default function App() {
   return (
-    <AuthProvider> {/* ✅ Context Wrapper — this is essential */}
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
           {/* Login Page */}
           <Route path="/" element={<Login />} />
 
-          {/* Change Password Page
-          <Route path="/change-password" element={<ChangePasswordPage />} /> */}
+          {/* Change Password Page */}
+          <Route
+            path="/change-password"
+            element={
+              <ProtectedRoute allowedRoles={["student", "admin"]}>
+                <FirstTimeChangePassword />
+              </ProtectedRoute>
+            }
+          />
 
           {/* Student Dashboard */}
           <Route
